d7a26c42e11e1aefa21b6b1cae28009a8cd83a5a
Ian <152772a@mymail.nyp.edu.sg>
2018-06-18 03:41:44 +0800

    convert file to binary and upload as stream

--- app/src/main/java/FacialRecognition/FacialRecognitionConfiguration.java
+++ app/src/main/java/FacialRecognition/FacialRecognitionConfiguration.java
@@ -1,11 +1,18 @@
 package FacialRecognition;
 
+import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import okhttp3.FormBody;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -14,11 +21,25 @@ import okhttp3.Response;
 public class FacialRecognitionConfiguration {
 
 
-    public static void main (String[]args){
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static void main (String[]args) throws IOException {
         OkHttpClient client = new OkHttpClient();
         //application/octet-stream
-        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        RequestBody body = RequestBody.create(JSON, "{\"url\":\"https://i.imgur.com/9sAyPWE.jpg\"}");
+//        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+//        RequestBody body = RequestBody.create(JSON, "{\"url\":\"https://i.imgur.com/9sAyPWE.jpg\"}");
+
+//        File file = new File("D:\\AndroidStudio\\Epione_V1\\app\\src\\main\\assets\\images\\9sAyPWE.jpg");
+        Path path = Paths.get("D:\\AndroidStudio\\Epione_V1\\app\\src\\main\\assets\\images\\9sAyPWE.jpg");
+
+            byte[]bttest = Files.readAllBytes(path);
+
+
+        MediaType fdf = MediaType.parse("application/octet-stream");
+        RequestBody body = RequestBody.create(fdf, bttest);
+
+
+
+
 
 
 //        RequestBody formBody = new FormBody.Builder()

de4453486ad1ae1062be8559b75425d3d16b0a62
Ian <152772a@mymail.nyp.edu.sg>
2018-06-18 03:44:36 +0800

    convert file to binary and upload as stream

--- app/src/main/java/FacialRecognition/FacialRecognitionConfiguration.java
+++ app/src/main/java/FacialRecognition/FacialRecognitionConfiguration.java
@@ -1,18 +1,14 @@
 package FacialRecognition;
 
 import android.os.Build;
-import android.provider.Settings;
 import android.support.annotation.RequiresApi;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import okhttp3.FormBody;
 import okhttp3.MediaType;
-import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -22,7 +18,7 @@ public class FacialRecognitionConfiguration {
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public static void main (String[]args) throws IOException {
+    public static void main(String[] args) throws IOException {
         OkHttpClient client = new OkHttpClient();
         //application/octet-stream
 //        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
@@ -31,15 +27,11 @@ public class FacialRecognitionConfiguration {
 //        File file = new File("D:\\AndroidStudio\\Epione_V1\\app\\src\\main\\assets\\images\\9sAyPWE.jpg");
         Path path = Paths.get("D:\\AndroidStudio\\Epione_V1\\app\\src\\main\\assets\\images\\9sAyPWE.jpg");
 
-            byte[]bttest = Files.readAllBytes(path);
+        byte[] bttest = Files.readAllBytes(path);
 
 
         MediaType fdf = MediaType.parse("application/octet-stream");
         RequestBody body = RequestBody.create(fdf, bttest);
-
-
-
-
 
 
 //        RequestBody formBody = new FormBody.Builder()
@@ -50,7 +42,7 @@ public class FacialRecognitionConfiguration {
 
                 .post(body)
                 .addHeader("Content-Type", "application/json")
-                .addHeader("Ocp-Apim-Subscription-Key","f697d347019b4c74976466006f62d811")
+                .addHeader("Ocp-Apim-Subscription-Key", "f697d347019b4c74976466006f62d811")
                 .build();
 
         try {

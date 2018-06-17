2aec2083cdcf65e5233599ac1b7a3d2c53d09ea7
xlepotato <yucia181@gmail.com>
2018-06-18 03:21:30 +0800

    fv

--- app/src/main/java/FacialRecognition/FacialRecognitionConfiguration.java
+++ app/src/main/java/FacialRecognition/FacialRecognitionConfiguration.java
@@ -1,4 +1,44 @@
 package FacialRecognition;
 
+import android.provider.Settings;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class FacialRecognitionConfiguration {
+
+
+    public static void main (String[]args){
+        OkHttpClient client = new OkHttpClient();
+        //application/octet-stream
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(JSON, "{\"url\":\"https://i.imgur.com/9sAyPWE.jpg\"}");
+
+
+//        RequestBody formBody = new FormBody.Builder()
+//                .add("url","https://i.imgur.com/9sAyPWE.jpg")
+//                .build();
+        Request request = new Request.Builder()
+                .url("https://southeastasia.api.cognitive.microsoft.com/face/v1.0/detect")
+
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Ocp-Apim-Subscription-Key","f697d347019b4c74976466006f62d811")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response.body().string());
+            // Do something with the response.
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
